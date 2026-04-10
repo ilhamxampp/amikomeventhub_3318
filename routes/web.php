80df70304fc0f6dae63f6404516b759871c1cbe4
ilhamxampp <ilhamrazaq@students.amikom.ac.id>
2026-04-10 12:08:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/kontak', function () {
+return view('kontak');
+});
+
+Route::get('/katalog', function () {
+return view('katalog');
+});
+
+Route::get('/profil', function () {
+return view('profil');
+});
+
+Route::get('/bantuan', function () {
+return view('bantuan');
+});
