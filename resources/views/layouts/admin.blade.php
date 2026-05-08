9b6722a08c43256787bbf16bfc121216454c7ae3
ilhamxampp <ilhamrazaq@students.amikom.ac.id>
2026-05-08 10:37:04 +0700

    tugas 4

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,6 +21,15 @@
             <a href="{{ route('admin.events.index') }}" class="flex items-center gap-3 px-4 py-3 {{ request()->routeIs('admin.events.*') ? 'bg-indigo-800 text-white' : 'hover:bg-indigo-800' }} rounded-xl font-bold transition">
                 Kelola Event
             </a>
+            <a href="#"
+        class="flex items-center gap-3 px-4 py-3 hover:bg-indigo-800 rounded-xl font-bold transition">
+        <svg class="w-5 h-5 text-indigo-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a２ ２ ０ ０１－２ ２Ｈ６ａ２ ２ ０ ０１－２－２ｖ－２ｚ">
+          </path>
+        </svg>
+        Laporan Transaksi
+      </a>
         </nav>
     </aside>
 
